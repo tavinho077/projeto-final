0cad02b036c6af1436947952564df4d5e1f5ac8e
tavinho077 <otavioaugusto2177@gmail.com>
2024-11-18 09:08:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,77 @@
+criaCartao(
+    'futebol',
+    'qual o time com mais paulistao?',
+    'corinthians'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'futrbol',
+    'qual o time que nao tem mundial?',
+    'palmeiras'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'futebol',
+    'qual o time que foi rebaixado em 2023?',
+    'santos)'
+)
+
+criaCartao(
+    'futebol',
+    'na onde o neymar joga?',
+    'al hilal'
+)
+
+criaCartao(
+    'futebol',
+    'qual o maior time do brasil?',
+    '´corinthians 
+)
+
+criaCartao(
+    'geografia',
+    'qual a capital da russia?',
+    '´moscou  
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
